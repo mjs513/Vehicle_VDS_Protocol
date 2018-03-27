2bbad973f17af80c9b649c4249a5b2c7dc2b5995
Yuanxin Mu <1051890136@qq.com>
2018-03-27 09:41:38 +0800

    Add files via upload
    command

---
+++ command/command.h
@@ -0,0 +1,18 @@
+/**********************************************************************
+Copyright:		YiXiong S&T Co., Ltd.
+Description:	声明与命令或具体功能配置相关的全局或局部变量
+History:
+	<author>	<time>		<desc>
+
+************************************************************************/
+
+#ifndef _PROTOCOL_CMD_H
+#define _PROTOCOL_CMD_H
+#include "../interface/protocol_define.h"
+
+extern  STRUCT_CMD g_stCmdList[];
+extern  int	g_iQuitActuatorTestCmdIndex[];
+
+
+
+#endif
